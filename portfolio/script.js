49632fc8e5af9ea0842626320f3d8cee8bfd2242
henrique-fullstack <carloshenriquenunesexu@gmail.com>
2025-12-25 18:34:46 -0300

    CSS apdate

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,18 @@
-//Arquivo JS separado para portfolio.html
+const botaoMensagem = document.getElementById('mostrar-mensagem-btn');
+const divMensagem = document.getElementById('mensagem-js');
+
+botaoMensagem.addEventListener('click', () => {
+    // Limpa mensagens anteriores
+    divMensagem.innerHTML = ''; 
+
+    const novaMensagem = document.createElement('p');
+    novaMensagem.textContent = 'Hello World! Bem-vindos ao meu Portfólio!';
+    novaMensagem.classList.add('mensagem-animada'); // Adiciona a classe para animar
+
+    divMensagem.appendChild(novaMensagem);
+
+    // Opcional: Remover a mensagem após alguns segundos
+    setTimeout(() => {
+        novaMensagem.remove();
+    }, 5000); 
+});
