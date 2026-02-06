e4d0c50a141855c9c0753be96be9ceba8378a725
Henrique_dev <henrique.dev.fullstack@gmail.com>
2026-02-05 21:48:34 -0300

    add save transaction an open database

--- database.py
+++ database.py
@@ -53,7 +53,7 @@ def save_user(username, id_hex, position='users'):
     cursor = connection.cursor()
     try:
         cursor.execute(
-            "INSERT INTO users (username, id_hex, position) VALUES (?, ?, ?)",
+            "INSERT INTO users (username, id_hex, registration_date, position) VALUES (?, ?, CURRENT_TIMESTAMP, ?)",
             (username, id_hex, position)
         )
         connection.commit()
@@ -62,3 +62,39 @@ def save_user(username, id_hex, position='users'):
         print(f"Erro ao salvar usuário: {e}")
     finally:
         connection.close()
+
+def save_transaction(id_users, value, category=None):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    try:
+        cursor.execute(
+            "INSERT INTO transactions (id_users, value, category) VALUES (?, ?, ?,)",
+            (id_users, value, category)
+        )
+        connection.commit()
+        print(f"Transação para o usuário {id_users} salva com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro ao salvar transação: {e}")
+    finally:
+        connection.close()
+
+
+def open_database(table_name, id_hex=None):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    print(f"--- ACESSANDO O SISTEMA ---")
+
+    if id_hex is None:
+        print(f"Exibindo todos os registros da tabela '{table_name}':")
+        cursor.execute(f"SELECT * FROM {table_name}")
+        records = cursor.fetchall()
+        for record in records:
+            print(record)
+    else:
+        print(f"Exibindo registros para o id_hex '{id_hex}' na tabela '{table_name}':")
+        cursor.execute(f"SELECT * FROM {table_name} WHERE id_hex = ?", (id_hex,))
+        records = cursor.fetchall()
+        for record in records:
+            print(record)
+    print("---------------------------")
+    connection.close()
